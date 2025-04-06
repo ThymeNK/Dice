4a3f5ed0c748bc53d886c485203627b354d1bffb
Timothy Tao <thy20030626@gmail.com>
2025-04-06 19:01:50 +0800

    新增摇动功能，震动反馈，初始随机骰子。

--- Dice/ViewController.swift
+++ Dice/ViewController.swift
@@ -9,23 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let diceArr = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
     var index1: Int = 1
+    var index2: Int = 1
 
     @IBOutlet weak var deiceImageView1: UIImageView!
     
     @IBOutlet weak var deiceImageView2: UIImageView!
     
     @IBAction func roll(_ sender: Any) {
-        
-        index1 = Int.random(in: 1...6)
-        
-        deiceImageView1.image = UIImage(named: "dice\(index1)")
-        deiceImageView2.image = UIImage(named: "dice\(Int.random(in: 1...6))")
+        updateDiceImages()
+        UIImpactFeedbackGenerator(style: .light).impactOccurred()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        updateDiceImages()
+    }
+    
+    func updateDiceImages() {
+        index1 = Int.random(in: 1...6)
+        index2 = Int.random(in: 1...6)
+        
+        deiceImageView1.image = UIImage(named: "dice\(index1)")
+        deiceImageView2.image = UIImage(named: diceArr[index2 - 1])
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        updateDiceImages()
     }
 
 
